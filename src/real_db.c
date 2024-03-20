609747a180485d2ac41210917b448ae5b2105fac
Sergey Savushkin <savushkin.sv@yandex.ru>
2024-03-20 20:58:41 +0300

    add db_remove()

--- src/real_db.c
+++ src/real_db.c
@@ -10,29 +10,16 @@
 #define MENU_SHOWALL 2
 #define MENU_ADD 3
 #define MENU_REMOVE 4
+#define DB_NAME_FILEPATH "../data-samples/task007.db"
 
 static char *menu_command[MENU_SIZE] = {"EXIT", "SHOW", "SHOWALL", "ADD", "REMOVE"};
-
-static int callback(void *const data, int argc, char **argv, char **azColName) {
-    if (!data) {
-        fprintf(stderr, "%s: ", azColName[0]);
-    }
-
-    for (int i = 0; i < argc; i++) {
-        if (i == argc - 1)
-            printf("%s", argv[i] ? argv[i] : "NULL");
-        else
-            printf("%s ", argv[i] ? argv[i] : "NULL");
-    }
-    printf("\n");
-
-    return 0;
-}
 
 void run();
 int get_command();
 char *get_string();
 void db_show_all();
+void db_show();
+void db_remove();
 
 int main() {
     run();
@@ -47,7 +34,7 @@ void run() {
             case MENU_EXIT:
                 break;
             case MENU_SHOW:
-                printf("MENU_SHOW\n");
+                db_show();
                 break;
             case MENU_SHOWALL:
                 db_show_all();
@@ -56,7 +43,7 @@ void run() {
                 printf("MENU_ADD\n");
                 break;
             case MENU_REMOVE:
-                printf("MENU_REMOVE\n");
+                db_remove();
                 break;
             default:
                 break;
@@ -104,21 +91,90 @@ char *get_string() {
 }
 
 void db_show_all() {
-    sqlite3 *db;  // указатель на базу данных
-    // открываем подключение к базе данных
-    int rc = sqlite3_open("../data-samples/task007.db", &db);
+    sqlite3 *db;        // указатель на базу данных
+    sqlite3_stmt *res;  // компилируемое выражение
+    int rc = sqlite3_open(DB_NAME_FILEPATH, &db);
     if (rc) {
         fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
     } else {
-        char *zErrMsg = 0;
-        const char *data = "";
         char *sql =
             "SELECT id, Name, Age, email \
-            from Students \
+            FROM Students \
             ORDER BY id";
-        // rc = sqlite3_exec(db, sql, callback, (void *)data, &zErrMsg);
-        sqlite3_exec(db, sql, callback, (void *)data, &zErrMsg);
+        rc = sqlite3_prepare_v2(db, sql, -1, &res, 0);
+        if (rc == SQLITE_OK) {
+            int step = 0;
+            while ((step = sqlite3_step(res)) == SQLITE_ROW) {
+                printf("%d %s %d %s\n", sqlite3_column_int(res, 0), sqlite3_column_text(res, 1),
+                       sqlite3_column_int(res, 2), sqlite3_column_text(res, 3));
+            }
+        } else {
+            fprintf(stderr, "Error: %s\n", sqlite3_errmsg(db));
+        }
     }
-    // закрываем подключение
+    sqlite3_finalize(res);  // удаляем скомпилированное выражение
+    sqlite3_close(db);      // закрываем подключение
+}
+
+void db_show() {
+    sqlite3 *db;        // указатель на базу данных
+    sqlite3_stmt *res;  // компилируемое выражение
+    int rc = sqlite3_open(DB_NAME_FILEPATH, &db);
+    if (rc) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+    } else {
+        char *id = get_string();
+        char *sql =
+            "SELECT id, Name, Age, email \
+            FROM Students \
+            WHERE id=?";
+        rc = sqlite3_prepare_v2(db, sql, -1, &res, 0);
+        if (rc == SQLITE_OK) {
+            sqlite3_bind_text(res, 1, id, -1, SQLITE_STATIC);  // привязываем параметр
+            int step = sqlite3_step(res);                      // выполняем выражение
+            if (step == SQLITE_ROW) {  // если выражение успешно выполнено
+                printf("%d %s %d %s\n", sqlite3_column_int(res, 0), sqlite3_column_text(res, 1),
+                       sqlite3_column_int(res, 2), sqlite3_column_text(res, 3));
+            } else {
+                printf("NO DATA\n");
+            }
+
+        } else {
+            fprintf(stderr, "Error: %s\n", sqlite3_errmsg(db));
+        }
+        free(id);
+    }
+    sqlite3_finalize(res);  // удаляем скомпилированное выражение
+    sqlite3_close(db);
+}
+
+void db_remove() {
+    sqlite3 *db;
+    sqlite3_stmt *res;
+    int rc = sqlite3_open(DB_NAME_FILEPATH, &db);
+    if (rc) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+    } else {
+        char *id = get_string();
+        char *sql =
+            "DELETE \
+            from Students \
+            WHERE id=?";
+        rc = sqlite3_prepare_v2(db, sql, -1, &res, 0);
+        if (rc == SQLITE_OK) {
+            sqlite3_bind_text(res, 1, id, -1, SQLITE_STATIC);
+            int step = sqlite3_step(res);
+            if (step == SQLITE_ROW) {
+                printf("%d %s %d %s\n", sqlite3_column_int(res, 0), sqlite3_column_text(res, 1),
+                       sqlite3_column_int(res, 2), sqlite3_column_text(res, 3));
+            } else {
+                printf("NO DATA\n");
+            }
+
+        } else {
+            fprintf(stderr, "Error: %s\n", sqlite3_errmsg(db));
+        }
+        free(id);
+    }
     sqlite3_close(db);
 }
